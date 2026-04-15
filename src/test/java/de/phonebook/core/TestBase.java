ad466b33f536fef47b407c62d7b8f33258de1f35
KristinaTomash <kristitomsh@gmail.com>
2026-04-15 13:54:54 +0300

    Add 4.2

--- src/test/java/de/phonebook/core/TestBase.java
+++ src/test/java/de/phonebook/core/TestBase.java
@@ -1,18 +1,22 @@
 package de.phonebook.core;
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager();
-
-    @BeforeMethod
+    protected static ApplicationManager app = new ApplicationManager
+            (System.getProperty("browser", Browser.CHROME.browserName()));
+@BeforeSuite
+   // @BeforeMethod
     public void setUp() {
         app.init();
     }
-
-    @AfterMethod(enabled = true)
+@AfterSuite
+    //@AfterMethod(enabled = true)
     public void tearDown() {
         app.stop();
     }
